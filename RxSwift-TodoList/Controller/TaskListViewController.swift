330557cedb4795b3777e2aa8a5c2030a0497337b
Dai Kawahara <skbsporta7@gmail.com>
2019-11-02 13:29:02 +0900

    add filter task at segmentcontrol

--- RxSwift-TodoList/Controller/TaskListViewController.swift
+++ RxSwift-TodoList/Controller/TaskListViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class TaskListViewController: UIViewController {
 
@@ -15,6 +16,8 @@ class TaskListViewController: UIViewController {
     @IBOutlet weak private var segmentedControl: UISegmentedControl!
 
     private var disposeBag = DisposeBag()
+    private var tasks = BehaviorRelay<[Task]>(value: [])
+    private var filterTasks = BehaviorRelay<[Task]>(value: [])
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +29,21 @@ class TaskListViewController: UIViewController {
             fatalError()
         }
 
-        addTaskVC.taskSubjectObservable.subscribe(onNext: { task in
-            print(task)
+        addTaskVC.taskSubjectObservable.subscribe(onNext: { [weak self] task in
+            guard let self = self else { return }
+            self.tasks.accept(self.tasks.value + [task])
+            if let selectedPriority = Priority(rawValue: self.segmentedControl.selectedSegmentIndex - 1) {
+                self.filterTasks.accept(self.filterTask(by: selectedPriority, tasks: self.tasks.value))
+            } else {
+                // allのケース
+                self.filterTasks.accept(self.tasks.value)
+            }
+            self.tableView.reloadData()
         }).disposed(by: disposeBag)
+    }
+
+    func filterTask(by selectedPriority: Priority, tasks: [Task]) -> [Task] {
+        return tasks.filter { $0.priority == selectedPriority }
     }
 }
 
@@ -38,7 +53,8 @@ extension TaskListViewController: UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        print(filterTasks.value.count)
+        return filterTasks.value.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
